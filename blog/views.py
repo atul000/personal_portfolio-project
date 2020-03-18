72cbb1e3aa5c0657d54b0d1f0901ccdd60c75a4c
atul000 <atulbeniwal15@gmail.com>
2020-03-18 15:16:19 +0530

    blog

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from .models import Blog
 
 
 def all_blogs(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-date')[:5]
     return render(request, 'blogs/all_blogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    blog = get_list_or_404(Blog, pk=blog_id)
+    return render(request, 'blogs/detail.html', {'blog': blog})
